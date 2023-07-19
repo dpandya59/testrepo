edf2889bcd5f14eb3cf7bbbdbbbf8dfc3fd8da0e
dpandya59 <76833371+dpandya59@users.noreply.github.com>
2023-07-19 06:26:07 +0530

    Assigment1

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1>Data Science Tools and Ecosystem</h1>
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+# 
+
+# <b>Objectives:</b>
+# 
+# <li> List of Popular Languages in Data Science</li>
+# <li> Different Open Source Tools used in Data Science </li>
+# <li> Sample Arithmetic Operation</li>
+#     
+# </ul>
+
+# Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. C
+# 5. JAVASCRIPT
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. Pytorch
+# 2. Pandas
+# 3. TensorFlow
+# 4. scikit-learn
+
+# <!DOCTYPE html>
+# <html>
+# <style>
+# table, th, td {
+#   border:1px solid black;
+# }
+# </style>
+# <body>
+# 
+# 
+# <table >
+#   <tr>
+#     <th>Data Science Tools</th>
+#   </tr>
+#   <tr>
+#     <td>Jupyter</td>
+# 
+#   </tr>
+#   <tr>
+#     <td>Jupyter Lab</td>
+#   </tr>
+#   <tr>
+#     <td>Apache zeppelin</td>
+#   </tr>
+#   <tr>
+#     <td>RStudio</td>
+#   </tr>
+#    <tr>
+#     <td>Spyder</td>
+#   </tr>
+# </table>
+# 
+# </body>
+# </html>
+# 
+# 
+
+# <h3>Below are a few examples of evaluating arithmetic expressions in Python.</h3>
+# z=x+y
+# z=x-y
+# z=3
+# a=x/3
+
+# In[1]:
+
+
+# This a simple arithmetic expression to mutiply then add integers.
+(3*4)+5
+
+
+# In[2]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+200/60
+
+
+# <h2>Author </h2>
+# Deepak Pandya
+# 
+
+# In[ ]:
+
+
+
+
